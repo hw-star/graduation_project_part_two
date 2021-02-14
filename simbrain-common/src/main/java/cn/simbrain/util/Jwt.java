9d2968a2fe52db0b881166bceb22d0a8bd447176
hw-star <3111314916@qq.com>
2021-02-14 21:31:56 +0800

    增加jwt令牌功能

---
+++ simbrain-common/src/main/java/cn/simbrain/util/Jwt.java
@@ -0,0 +1,62 @@
+package cn.simbrain.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @author huowei
+ * @version 1.0.0
+ * @description Jwt令牌
+ * @date 2021/2/14
+ */
+@Component
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ConfigurationProperties("simbrain.jwt.config")
+public class Jwt {
+    /**
+     * 密钥
+     */
+    private String secretKey = "simbrain";
+    /**
+     * 令牌有效时间(默认7天)
+     */
+    private long expires = 60 * 60 * 24 * 7;
+
+    /**
+     * @description: 生成令牌
+     * @Param id: 账号
+     * @Param name: 名字
+     * @Param isLogin: 是否登录
+     * @return: java.lang.String
+     */
+    public String createJwt(String id, String name, Boolean isLogin){
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+        JwtBuilder jwtBuilder = Jwts.builder().setId(id)
+                .setSubject(name)
+                .setIssuedAt(now)
+                .signWith(SignatureAlgorithm.HS256,secretKey)
+                .claim("isLogin",isLogin);
+        if (expires > 0){
+            jwtBuilder.setExpiration(new Date(nowMillis + expires*1000));
+        }
+        return jwtBuilder.compact();
+    }
+
+    public Claims parseJwt(String jwtToken){
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken).getBody();
+    }
+
+
+}
--- simbrain-service/src/main/java/cn/simbrain/controller/UserController.java
+++ simbrain-service/src/main/java/cn/simbrain/controller/UserController.java
@@ -3,13 +3,18 @@ package cn.simbrain.controller;
 import cn.simbrain.mapper.UserMapper;
 import cn.simbrain.pojo.User;
 import cn.simbrain.provide.EmailProvide;
+import cn.simbrain.util.Jwt;
 import cn.simbrain.util.Result;
 import cn.simbrain.util.ResultCode;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.mysql.jdbc.StringUtils;
+import io.jsonwebtoken.Claims;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author huowei
@@ -25,6 +30,8 @@ public class UserController {
     private UserMapper userMapper;
     @Autowired
     private EmailProvide emailProvide;
+    @Autowired
+    private Jwt jwt;
 
     /**
      * @description: 测试查询单个用户数据
@@ -59,5 +66,29 @@ public class UserController {
         if (sendState)
             return Result.success();
         return Result.failure(ResultCode.SYSTEM_INNER_ERROR);
+    }
+
+    @PostMapping("/login")
+    public Result userLogin(@RequestParam("id") Long userIdL,@RequestParam("pwd") String userPwd){
+        String userId = String.valueOf(userIdL);
+        if (StringUtils.isNullOrEmpty(userId) || StringUtils.isNullOrEmpty(userPwd)){
+            return Result.failure(ResultCode.USER_LOGIN_ERROR);
+        }
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        wrapper.ge("user_id",userId);
+        User user = userMapper.selectOne(wrapper);
+        if (user == null){
+            return Result.failure(ResultCode.USER_LOGIN_ERROR);
+        }
+        if (userPwd.equals(user.getUserPwd())){
+            String token = jwt.createJwt(userId,user.getUserName(),true);
+            Map<String,String> map = new HashMap<>();
+            map.put("token",token);
+            Claims claims = jwt.parseJwt(token);
+            map.put("解析出的账号",claims.getId());
+            map.put("解析出的名字",claims.getSubject());
+            return Result.success(map);
+        }
+        return Result.failure(ResultCode.USER_LOGIN_ERROR);
     }
 }
