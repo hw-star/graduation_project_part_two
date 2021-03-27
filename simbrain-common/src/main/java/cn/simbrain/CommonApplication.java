cafafeb0b800201746c2a0c61e1f9e7bd77f6245
hw-star <3111314916@qq.com>
2021-03-27 14:41:13 +0800

    打包部署

---
+++ simbrain-common/src/main/java/cn/simbrain/CommonApplication.java
@@ -0,0 +1,17 @@
+package cn.simbrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author huowei
+ * @version 1.0.0
+ * @description 公共
+ * @date 2021/2/11
+ */
+@SpringBootApplication
+public class CommonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CommonApplication.class);
+    }
+}
